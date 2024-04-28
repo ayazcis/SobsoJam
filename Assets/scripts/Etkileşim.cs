589d4d569a45b7559fdf3f6cdddde209db236142
Mert <mertuz15@gmail.com>
2024-04-28 12:42:56 +0300

    .

--- Assets/scripts/Etkileşim.cs
+++ Assets/scripts/Etkileşim.cs
@@ -6,7 +6,10 @@ public class Etkileşim : MonoBehaviour
 {
     [SerializeField] public GameObject tus;
     [SerializeField] public GameObject diyalog;
+    [SerializeField] public GameObject[] diyaloglar;
+    int currentDialog = 0;
     int isTriggered = 0;
+    int isStarted = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -36,6 +39,7 @@ public class Etkileşim : MonoBehaviour
             tus.gameObject.SetActive(false);
             diyalog.SetActive(true);
             Time.timeScale = 0;
+            isStarted = 1;
         }
 
     }
@@ -43,10 +47,29 @@ public class Etkileşim : MonoBehaviour
     private void Update()
     {
         CanvasOpen();
+        TalkWithNpc();
     }
 
     public void TimeScaleToNormal()
     {
         Time.timeScale = 1;
+    }
+
+    public void TalkWithNpc()
+    {
+
+        if(isStarted == 1 && Input.GetMouseButtonDown(0))
+        {
+            if(currentDialog + 1 < diyaloglar.Length)
+            {
+                diyaloglar[currentDialog].SetActive(false);
+                currentDialog++;
+                diyaloglar[currentDialog].SetActive(true);
+            }
+            else
+            {
+
+            }
+        }
     }
 }
