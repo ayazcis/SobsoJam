e03f1d813892cfa71a38b8bce5c24ab22686e34c
cisem Ayaz <ayazcisem@gmail.com>
2024-04-28 05:15:23 +0300

    bossfight

---
+++ Assets/scripts/BossFight.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossFight : MonoBehaviour
+{
+	public float damageTime = 2f;
+	public float detectionRadius = 5f;
+	public float talkRadius = 7f;
+	public float attackDistance = 1.6f;
+	public float runToEnemySpeed = 4.5f;
+	public GameObject player;
+	public Image enemyHeart;
+
+	public GameObject canvas;
+
+	public FightingBandit fightingBandit;
+	public int healthEnemy = 100;
+
+	public GameObject eLetter;
+	public GameObject canvasTalk;
+
+	private bool attacking = false;
+	private bool diedE=false;
+	private bool attackState = false;
+	[SerializeField] float m_speed = 4.0f;
+	[SerializeField] float m_jumpForce = 7.5f;
+
+	public Animator m_animator;
+	private Rigidbody2D m_body2d;
+	private Sensor_Bandit m_groundSensor;
+	private bool m_grounded = false;
+	private bool m_combatIdle = false;
+	private bool m_isDead = false;
+
+	public bool talked = false;
+	public int water;
+	public int food;
+
+	[SerializeField] public TMP_Text Text_water;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_animator = GetComponent<Animator>();
+		m_body2d = GetComponent<Rigidbody2D>();
+		m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
+	
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Vector2.Distance(transform.position, player.transform.position) <= talkRadius)
+		{
+			eLetter.SetActive(true);
+			if (Input.GetKeyDown("e"))
+			{
+				eLetter.SetActive(false);
+				canvasTalk.SetActive(true);
+			}
+		}
+		if (talked)
+		{
+			enemyHeart.fillAmount = (float)healthEnemy / 100f;
+			if (enemyHeart.fillAmount == 0f)
+			{
+				canvas.SetActive(false);
+			}
+			if (healthEnemy <= 0)
+			{
+				diedE = true;
+				m_animator.SetTrigger("Death");
+				Destroy(gameObject, 1f);
+			}
+
+			if (Vector2.Distance(transform.position, player.transform.position) <= detectionRadius && !diedE)
+			{
+				attackState = true;
+			}
+			else if (!diedE)
+			{
+				attackState = false;
+				m_animator.SetInteger("AnimState", 0);
+				m_animator.SetBool("attackEnded", true);
+				m_animator.SetBool("attacking", false);
+				attacking = false;
+
+			}
+			if (attackState && !diedE)
+			{
+				// Move towards the enemy if in attack state and not within stopping distance
+				if (Vector2.Distance(transform.position, player.transform.position) > attackDistance)
+				{
+					Vector2 direction = (player.transform.position - transform.position).normalized;
+
+					if (direction.x < 0f)
+					{
+						transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+					}
+					else
+					{
+						transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+					}
+
+					transform.Translate(direction * runToEnemySpeed * Time.deltaTime);
+					m_animator.SetInteger("AnimState", 2);
+					m_animator.SetBool("attackEnded", true);
+					m_animator.SetBool("attacking", false);
+					attacking = false;
+
+				}
+				if (Vector2.Distance(transform.position, player.transform.position) < attackDistance)
+				{
+					m_animator.SetBool("attacking", true);
+					m_animator.SetInteger("AnimState", 1);
+					m_animator.SetBool("attackEnded", false);
+					attacking = true;
+				}
+
+
+			}
+		
+
+		}
+	}
+	public void DamageRecivePlayer()
+	{
+		if (Vector2.Distance(transform.position, player.transform.position) <= attackDistance)
+		{
+			fightingBandit.m_animator.SetTrigger("Hurt");
+			fightingBandit.health -= 10; // Her seferinde 10 hasar alacak, deðiþtirebilirsiniz
+			Debug.Log(fightingBandit.health + "  HEALTH ");
+		}
+			
+		
+	}
+
+
+}
---
+++ Assets/scripts/BossPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPlayer : MonoBehaviour
+{
+	[SerializeField] public GameObject tus;
+	[SerializeField] public GameObject diyalog;
+	int isTriggered = 0;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			isTriggered = 1;
+			tus.gameObject.SetActive(true);
+
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			tus.gameObject.SetActive(false);
+			isTriggered = 0;
+
+		}
+	}
+
+	void CanvasOpen()
+	{
+		if (isTriggered == 1 && Input.GetKeyDown("e"))
+		{
+			diyalog.SetActive(true);
+			Time.timeScale = 0;
+		}
+
+	}
+
+	private void Update()
+	{
+		CanvasOpen();
+	}
+
+	public void TimeScaleToNormal()
+	{
+		Time.timeScale = 1;
+	}
+}
+
