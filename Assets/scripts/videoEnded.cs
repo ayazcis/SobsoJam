7c285da9d3208f4e05fe0cd9dac21d505f576bf8
cisem Ayaz <ayazcisem@gmail.com>
2024-04-28 15:27:18 +0300

    end Scene

---
+++ Assets/scripts/videoEnded.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class videoEnded : MonoBehaviour
+{
+	public VideoPlayer videoPlayer;
+	
+
+	void Start()
+	{
+		// VideoPlayer bileþeninde "loopPointReached" event'ini dinleyerek video bitiþini algýla
+		videoPlayer.loopPointReached += OnVideoFinished;
+	}
+
+	void OnVideoFinished(VideoPlayer vp)
+	{
+		// Video bittiðinde sahne yükleme iþlemini gerçekleþtir
+		SceneManager.LoadScene(0);
+	}
+}
+
